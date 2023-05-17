d37e474a968a9889cfa30c1ea2168bbdd4b82c86
yichengjie <626659321@qq.com>
2023-05-17 13:07:14 +0800

    补充doc文档

--- hello-spark/src/main/java/com/yicj/spark/hello/HelloWorldApp.java
+++ hello-spark/src/main/java/com/yicj/spark/hello/HelloWorldApp.java
@@ -2,11 +2,7 @@ package com.yicj.spark.hello;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
-
-import java.util.Properties;
-
 import static org.apache.spark.sql.functions.concat;
 import static org.apache.spark.sql.functions.lit;
 
@@ -18,14 +14,6 @@ public class HelloWorldApp {
 
     //https://github.com/jgperrin/net.jgp.books.spark.ch01
     public static void main(String[] args) {
-        HelloWorldApp app = new HelloWorldApp() ;
-        app.start();
-    }
-
-    /**
-     * The processing code.
-     */
-    private void start() {
         // Creates a session on a local master
         SparkSession spark = SparkSession.builder()
                 .appName("CSV to Dataset")
@@ -35,20 +23,7 @@ public class HelloWorldApp {
         // dataframe
         Dataset<Row> df = spark.read().format("csv")
                 .option("header", "true")
-                .load("D:\\opt\\spark\\helloworld\\input\\data.csv");
-        Dataset<Row> ndf = df.withColumn("name",
-                concat(df.col("product_code"), lit(","), df.col("amount")));
-        // Shows at most 5 rows from the dataframe
-        ndf.show(5);
-        //
-        String url = "jdbc:mysql://localhost:3306/imoocdemo?characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai" ;
-        String table = "tbl_order_1" ;
-        final Properties properties = new Properties() ;
-        properties.put("driver", "com.mysql.cj.jdbc.Driver") ;
-        properties.put("user", "root") ;
-        properties.put("password", "root") ;
-        ndf.write()
-                .mode(SaveMode.Overwrite)
-                .jdbc(url, table, properties);
+                .load("D:\\data\\spark\\helloworld\\input\\data.csv");
+        df.show(5);
     }
 }
---
+++ hello-spark/src/main/java/com/yicj/spark/jdbc/HelloJdbcApp.java
@@ -0,0 +1,45 @@
+package com.yicj.spark.jdbc;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+/**
+ * @author yicj
+ * @date 2023年05月17日 12:59
+ */
+public class HelloJdbcApp {
+
+    public static void main(String[] args) {
+//        // Creates a session on a local master
+//        SparkSession spark = SparkSession.builder()
+//                .appName("CSV to Dataset")
+//                .master("local")
+//                .getOrCreate();
+//        // Reads a CSV file with header, called books.csv, stores it in a
+//        // dataframe
+//        Dataset<Row> df = spark.read().format("csv")
+//                .option("header", "true")
+//                .load("D:\\data\\spark\\helloworld\\input\\data.csv");
+//        Dataset<Row> ndf = df.withColumn("name",
+//                concat(df.col("product_code"), lit(","), df.col("amount")));
+//        // Shows at most 5 rows from the dataframe
+//        ndf.show(5);
+//        //
+//        String url = "jdbc:mysql://localhost:3306/imoocdemo?characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai" ;
+//        String table = "tbl_order_1" ;
+//        final Properties properties = new Properties() ;
+//        properties.put("driver", "com.mysql.cj.jdbc.Driver") ;
+//        properties.put("user", "root") ;
+//        properties.put("password", "root") ;
+//        ndf.write()
+//                .mode(SaveMode.Overwrite)
+//                .jdbc(url, table, properties);
+    }
+}
