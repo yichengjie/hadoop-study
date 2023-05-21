929fff51e5e01739a14d75c84362de97e46365bf
yichengjie <626659321@qq.com>
2023-05-21 13:39:48 +0800

    flink word to uppercase update

--- hello-flink/src/main/java/com/yicj/flink/hello/WordUpperCaseApp.java
+++ hello-flink/src/main/java/com/yicj/flink/hello/WordUpperCaseApp.java
@@ -12,7 +12,7 @@ import org.apache.flink.util.Collector;
  * @author: yicj
  * @date: 2023/5/20 16:07
  */
-public class DemoApp {
+public class WordUpperCaseApp {
 
     public static void main(String[] args) throws Exception {
         // 1.准备环境
@@ -20,28 +20,26 @@ public class DemoApp {
         // 设置运行模式
         env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
         // 2.加载数据源
-        DataStreamSource<String> elementsSource = env.fromElements("java,scala,php,c++",
-                "java,scala,php", "java,scala", "java");
-        // 数据转化
-        SingleOutputStreamOperator<String> flatMap = elementsSource.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public void flatMap(String element, Collector<String> out) throws Exception {
-                String[] wordArr = element.split(",");
-                for (String word : wordArr) {
-                    out.collect(word);
-                }
-            }
-        });
+        DataStreamSource<String> elementsSource = env.fromElements(
+                "java,scala,php,c++", "java,scala,php", "java,scala", "java");
+        //3. 数据转化
+        SingleOutputStreamOperator<String> flatMap = elementsSource.flatMap(new MyFlatMapper());
         // DataStream下边为DataStream的子类
-        SingleOutputStreamOperator<String> source = flatMap.map(new MapFunction<String, String>() {
-            @Override
-            public String map(String value) throws Exception {
-                return value.toUpperCase();
-            }
-        });
-        // 数据输出
+        SingleOutputStreamOperator<String> source = flatMap.map((MapFunction<String, String>) String::toUpperCase);
+        //4. 数据输出
         source.print() ;
-        // 执行程序
+        //5. 执行程序
         env.execute() ;
+    }
+
+    static class MyFlatMapper implements FlatMapFunction<String, String>{
+
+        @Override
+        public void flatMap(String value, Collector<String> out) throws Exception {
+            String[] wordArr = value.split(",");
+            for (String word : wordArr) {
+                out.collect(word);
+            }
+        }
     }
 }
