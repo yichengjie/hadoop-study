c1cd805fb9abb4e48f499bbaec52aec63f542d05
yichengjie <626659321@qq.com>
2023-05-21 17:07:11 +0800

    flink word count stream app

---
+++ hello-flink/src/main/java/com/yicj/flink/hello/WordCountSqlApp.java
@@ -0,0 +1,14 @@
+package com.yicj.flink.hello;
+
+/**
+ * @author: yicj
+ * @date: 2023/5/21 14:39
+ */
+public class WordCountSqlApp {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
